782eab66c14ee2bc52988ebfd1ae4677e487d619
Mohamed-Ayman-Fahmy <muhamed.ayman.fahmy@gmail.com>
2025-11-18 21:59:45 +0200

    add admin layout

---
+++ joint-clinic-frontend/src/app/(admin)/layout.tsx
@@ -0,0 +1,16 @@
+// src/app/(admin)/layout.tsx
+
+import React from "react";
+
+export default function AdminLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <section>
+            {/* Admin Shell / Sidebar / Header */}
+            {children}
+        </section>
+    );
+}
